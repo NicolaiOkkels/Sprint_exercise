0d8cbe74e48fd9bcfac5c6e311c65566a0d8b0ba
JooergenKoeldal <joer1894@stud.kea.dk>
2021-03-11 09:46:58 +0100

    add date to media

--- src/main/java/com/example/demo/model/Media.java
+++ src/main/java/com/example/demo/model/Media.java
@@ -1,13 +1,18 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Media {
 
     private long mediaId;
     private String mediaName;
+    private Date mediaDate;
 
-    public Media(long mediaId, String mediaName) {
+    public Media(long mediaId, String mediaName, Date mediaDate) {
         this.mediaId = mediaId;
         this.mediaName = mediaName;
+        this.mediaDate = mediaDate;
+
     }
 
     public long getMediaId() {
@@ -16,5 +21,9 @@ public class Media {
 
     public String getMediaName() {
         return mediaName;
+    }
+
+    public Date getMediaDate() {
+        return mediaDate;
     }
 }
