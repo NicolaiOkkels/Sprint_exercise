a3e166a3465c67033392bdf677d7218aed1615dc
Rasmus <Rasmusmberg@gmail.com>
2021-03-11 11:03:31 +0100

    Book Read All Service

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -1,4 +1,19 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+
+    @Autowired
+    BookRepository bookRepo;
+
+    public List<Book> readAllBooks(){
+        return bookRepo.readAllBooks();
+    }
 }
