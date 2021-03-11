ab7f733b1aa3196868795d4918f6484e16201489
kootasu <kootasu@hotmail.com>
2021-03-11 11:14:34 +0100

    MediaRepository, MediaService

--- src/main/java/com/example/demo/repository/MediaRepository.java
+++ src/main/java/com/example/demo/repository/MediaRepository.java
@@ -1,4 +1,43 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Media;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class MediaRepository {
+
+    @Autowired
+    org.springframework.jdbc.core.JdbcTemplate JdbcTemplate;
+
+    public List<Media> readAll(){
+        String sql = "SELECT * FROM media";
+        RowMapper<Media> rowMapper = new BeanPropertyRowMapper<>(Media.class);
+        return JdbcTemplate.query(sql, rowMapper);
+    }
+
+    public void create(Media media){
+        String sql = "INSERT INTO media (mediaId, mediaName, mediaDate) VALUES (?, ?, ?)";
+        JdbcTemplate.update(sql, media.getMediaId(), media.getMediaName(), media.getMediaDate());
+    }
+
+    public void delete(long mediaId){
+        String sql = "DELETE FROM media WHERE mediaId=?";
+        JdbcTemplate.update(sql, mediaId);
+    }
+
+    public Media read(long mediaId){
+        String sql = "SELECT * FROM media WHERE mediaId=?";
+        RowMapper<Media> rowMapper = new BeanPropertyRowMapper<>(Media.class);
+        return JdbcTemplate.queryForObject(sql, rowMapper, mediaId);
+    }
+
+    public void update(Media media){
+        String sql = "UPDATE media SET mediaName=?, mediaDate=? WHERE mediaId=?";
+        JdbcTemplate.update(sql, media.getMediaName(), media.getMediaDate(), media.getMediaId());
+    }
 }
--- src/main/java/com/example/demo/service/MediaService.java
+++ src/main/java/com/example/demo/service/MediaService.java
@@ -1,4 +1,37 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Media;
+import com.example.demo.repository.MediaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class MediaService {
+
+    @Autowired
+    MediaRepository mediaRepository;
+
+    public void create(Media media){ mediaRepository.create(media); }
+
+    public void delete(long mediaId){ mediaRepository.delete(mediaId); }
+
+    public void update(Media media){
+        mediaRepository.update(media);
+    }
+
+    public List<Media> readAll(){
+        List<Media> mediaList = new ArrayList<>();
+        for (Media media : mediaRepository.readAll()) {
+            mediaList.add(media);
+        }
+        return mediaList;
+    }
+
+    public Media read(long mediaId){
+        return mediaRepository.read(mediaId);
+    }
+
 }
