cf472968c5f3c9d1a8ce82a9162466c7d91cb9c3
Rasmus <Rasmusmberg@gmail.com>
2021-03-11 09:46:13 +0100

    Initial HomeController

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,4 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
