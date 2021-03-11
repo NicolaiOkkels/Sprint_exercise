b5c10e21fce29a9633270984d81da97f5dfdd10a
Rasmus <Rasmusmberg@gmail.com>
2021-03-11 10:58:12 +0100

    Book Repository

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -6,6 +6,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String index(){
-        return "index";
+        return "login";
     }
 }
--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,4 +1,36 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class BookRepository {
+
+    @Autowired
+    JdbcTemplate template;
+
+    public Book findBookById(int id){
+        String sql = "SELECT * FROM books WHERE book_id = ?";
+        RowMapper<Book> rowMapper = new BeanPropertyRowMapper<>(Book.class);
+        Book book = template.queryForObject(sql, rowMapper, id);
+        return book;
+    }
+
+    public List<Book> readAllBooks(){
+        String sql = "SELECT * FROM books;";
+        RowMapper<Book> rowMapper = new BeanPropertyRowMapper<>(Book.class);
+        return template.query(sql, rowMapper);
+    }
+
+    public Book addBook(Book book){
+        String sql = "INSERT INTO books (id, name, date) VALUES (?, ?, ?);";
+        template.update(sql, book.getId(), book.getName());
+        return null;
+    }
 }
